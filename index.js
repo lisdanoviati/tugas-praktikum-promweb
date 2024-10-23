e47d078ada597ee99f052c56fef7b392ab5005be
lisdanoviati <lisdanoviati36@gmail.com>
2024-10-23 15:48:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-document.getElementById("id-name").innerText ="KOM B"
-
-const age= document.getElementsByClassName("age")
-for (let i=0; i < age.leght; i++) {
-   age[i].innerText = "paragraf" + i
-}
-
-const lab= document.getElementsByTagName("h4")
-for (let i=0; i < lab.leght; i++) {
-    lab[i].innerText = "jaringan" + (i+1)
-}
+var typed = new Typed(".multiple-text", {
+    strings: ["Information Technology Student at the University of North Sumatra", "Information Technology Student at the University of North Sumatra", "Information Technology Student at the University of North Sumatra"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
